0179b640d2e5a1b767b443e56fd20233c0f83d02
francesbaldridge <yxy5dp@virginia.edu>
2025-01-26 11:09:08 -0500

    Update expected_points.R

--- expected_points.R
+++ expected_points.R
@@ -1,1 +1,7 @@
-
+expected_points <- function(down, ytg, fp, num_epochs = 100) {
+  results <- numeric(num_epochs)
+  for (i in 1:num_epochs) {
+    results[i] <- epoch(down, ytg, fp)
+  }
+  return(mean(results))
+}
