0e0ff82237290d3489c4f44f2691a6f6f63c3db4
francesbaldridge <yxy5dp@virginia.edu>
2025-01-29 10:08:17 -0500

    Update score.R

--- score.R
+++ score.R
@@ -1,9 +1,9 @@
 score <- function(state) {
   if (100 < state$fp & state$fp <= 110) {
-    return(7)
+    7
   } else if (110 < state$fp & state$fp <= 120) {
-    return(3)
+    3
   } else {
-    return(0)
+    0
   }
 }
