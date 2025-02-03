57d9d7dc1a447713ad99fb8b0669d182a24c4642
francesbaldridge <yxy5dp@virginia.edu>
2025-02-03 11:58:05 -0500

    Update and rename score.R to utils.R

--- score.R
+++
@@ -1,9 +0,0 @@
-score <- function(state) {
-  if (100 < state$fp & state$fp <= 110) {
-    7
-  } else if (110 < state$fp & state$fp <= 120) {
-    3
-  } else {
-    0
-  }
-}
---
+++ utils.R
@@ -0,0 +1,11 @@
+compute_score <- function(fp){
+  
+  if(fp <= 100) {
+    0
+  } else if (fp <= 110) {
+    7
+  } else {
+    3
+  }
+
+}
