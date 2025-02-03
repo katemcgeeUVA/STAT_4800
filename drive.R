7a4b39052d9aefb1834effab4cea7ec65cea7ee4
francesbaldridge <yxy5dp@virginia.edu>
2025-02-03 11:52:18 -0500

    Update drive.R

--- drive.R
+++ drive.R
@@ -1,6 +1,4 @@
-drive <- function(down, ytg, fp) {
-  new_fp <- fp + sample(-10:40, 1)
-  new_fp <- max(0, min(120, new_fp))
-  state <- list(down = 1, ytg = 10, fp = new_fp)
-  state
+run_drive <- function(down, ytg, fp) {
+  new_fp <- sample(c(80, 105, 115), 1, prob=c(.9, .05, .05))
+  list(down=1, ytg=10, fp=new_fp)
 }
