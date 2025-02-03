3c3ea191dec06a3bae6c2819f16ba0638ed3d6dc
francesbaldridge <yxy5dp@virginia.edu>
2025-02-03 11:56:18 -0500

    Update expected_points.R

--- expected_points.R
+++ expected_points.R
@@ -1,7 +1,18 @@
-expected_points <- function(down, ytg, fp, num_epochs = 100) {
-  results <- numeric(num_epochs)
-  for (i in 1:num_epochs) {
-    results[i] <- epoch(down, ytg, fp)
-  }
-  mean(results)
+source("epoch.R")
+
+get_EP <- function(down, ytg, fp) {
+ 
+  n <- 1000 
+  points <- rep(NA, n)
+  
+  for(i in 1:n) {
+    points[i] <- run_epoch(down, ytg, fp)
+  } 
+  
+  mean(points)
 }
+
+############################# Test that get_EP works ###########################
+
+get_EP(2, 5, 34)
+get_EP(1, 10, 61)
