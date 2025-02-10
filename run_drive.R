a9033ea0ce3c66448b38678f39d06f0a9da492b6
francesbaldridge <yxy5dp@virginia.edu>
2025-02-10 10:45:50 -0500

    Update run_drive.R

--- run_drive.R
+++ run_drive.R
@@ -1,4 +1,22 @@
-run_drive <- function(down, ytg, fp) {
-  new_fp <- sample(c(80, 105, 115), 1, prob=c(.9, .05, .05))
-  list(down=1, ytg=10, fp=new_fp)
+source("run_play.R")
+
+pbpdata <- readRDS("~/Desktop/Stat/pbp2014-2024.rds")
+
+run_drive <- function(down, ytg, fp){
+  
+  # get new state
+  new_state <- run_play(down, ytg, fp)
+  
+  # Check if we should return the state or run a new play
+  if(new_state$exit_drive==0){
+    
+    # if we should stay with the current drive, simply call it again with the
+    # new state
+    run_drive(new_state$down, new_state$ytg, new_state$fp)
+    
+  } else {
+    
+    # otherwise, return the current state to the run_epoch function
+    list(down=new_state$down, YTG=new_state$ytg, fp=new_state$fp)
+  }
 }
